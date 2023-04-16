efb217f8d228973e21d00407f636f5445cde53aa
Ian Bayne <ianbayne@gmail.com>
2023-04-17 08:31:11 +0900

    new: add esbuild

---
+++ esbuild.mjs
@@ -0,0 +1,25 @@
+import * as esbuild from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+
+const options = {
+  bundle: true,
+  entryPoints: ["./src/background.ts", "./src/content.ts"],
+  minify: process.env.NODE_ENV === "production",
+  outdir: "./dist",
+  plugins: [
+    copy({
+      // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+      // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+      // REF: https://www.npmjs.com/package/esbuild-plugin-copy
+      resolveFrom: "cwd",
+      assets: [
+        {
+          from: ["./manifest.json"],
+          to: ["./dist/manifest.json"],
+        },
+      ],
+    }),
+  ],
+};
+
+await esbuild.build(options);
