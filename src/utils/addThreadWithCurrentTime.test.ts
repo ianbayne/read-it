7b66cfc1393778b5d4a5fbed51b6f969f11deff6
Ian Bayne <ianbayne@gmail.com>
2023-05-02 18:17:39 +0900

    refactor: add todo

---
+++ src/utils/addThreadWithCurrentTime.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "@jest/globals";
+
+// import addThreadWithCurrentTime from "./addThreadWithCurrentTime";
+
+describe("addThreadWithCurrentTime()", () => {
+  it("adds the thread ID to Chrome storage with the current time", async () => {
+    // TODO: Figure out how to test Chrome storage
+  });
+});
