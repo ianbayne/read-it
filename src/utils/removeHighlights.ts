36e3eb6701ee4475bae2a63909ba8192ac4ecdd5
Ian Bayne <ianbayne@gmail.com>
2023-05-05 17:36:02 +0900

    refactor: standardize on how to assert types

--- src/utils/removeHighlights.ts
+++ src/utils/removeHighlights.ts
@@ -1,9 +1,9 @@
 import { HIGHLIGHT_CLASS_NAME } from "../constants";
 
 export default function removeHighlights(): void {
-  const highlightedComments = document.querySelectorAll(
-    `.comment.${HIGHLIGHT_CLASS_NAME}`
-  ) as NodeListOf<HTMLElement>;
+  const highlightedComments = <NodeListOf<HTMLElement>>(
+    document.querySelectorAll(`.comment.${HIGHLIGHT_CLASS_NAME}`)
+  );
   highlightedComments.forEach((highlightedComment) => {
     highlightedComment.style.backgroundColor = "";
   });
